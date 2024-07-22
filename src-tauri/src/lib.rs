361bee296587737be1fccd3c310ba5eaca1c41af
Sebastian Wang <43295730+Lemon2ee@users.noreply.github.com>
2024-07-22 07:42:43 -0400

    made deep link working, only after build on macos

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,39 @@
+use tauri::{AppHandle, Listener};
+use tauri_plugin_dialog::{DialogExt, MessageDialogKind};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_deep_link::init())
+        .setup(|app| {
+            println!("hello");
+            let handle = app.handle().clone();
+            app.listen("deep-link://new-url", move |event| {
+                deep_link(&event, &handle)
+            });
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn deep_link(event: &tauri::Event, app: &AppHandle) {
+    let payload = event.payload();
+    app.dialog()
+        .message("Tauri is Awesome")
+        .kind(MessageDialogKind::Info)
+        .title("Information")
+        .ok_button_label("Absolutely")
+        .show(|result| match result {
+            true => log::info!("received deep link"),
+            false => log::info!("received deep link")
+        });
+
+    let Some(_link) = payload.get(2..payload.len() - 2) else {
+        log::info!("received deep link");
+        return;
+    };
+    // Parse deep link
+    log::info!("received deep link");
 }
